758db7ba31d97a5f3f307069b973b9c15e022af2
iStrokes <felipebruckmann@hotmail.com.br>
2020-06-22 08:32:06 -0300

    desafio 3-2

---
+++ desafio-rocket-3/data.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id : "starter",
+        description: '<p><span> Seja um programador desejado</span><br>no mercado com esses cursos gratuitos</p>',
+        imgUrl: "/assets/starter.svg"
+    },
+    {
+        id: "launchbase",
+        description: ' <p><span>Domine a programação do zero</span> e tenha acesso ás melhores oportunidades no mercado</p>',
+        imgUrl: "/assets/launcbase.svg"
+    },
+    {
+        id: "gostack",
+        description: '<p><span>Treinamento imersivo</span> nas tecnologias mais modernas de desenvolvimento web e mobile</p>',
+        imgUrl: "assets/gostack.svg"
+    }
+]
--- desafio-rocket-3/server.js
+++ desafio-rocket-3/server.js
@@ -5,22 +5,31 @@ const nunjucks = require('nunjucks')
 server.use(express.static('public'))
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    noCache: true,
+    autoescape: false
 })
 
 server.set("view engine" , "njk")
 
 server.get("/" , (req, res) => {
-    return res.render("cursos")
+    const cursos = require('./data')
+
+    return res.render("cursos" , {cursos})
 })
 
 server.get("/sobre" , (req, res) => {
-    return res.render("sobre")
+    const sobre = {
+        img: "/assets/seat.jpg",
+        nome: "Rocketseat",
+        info: "<p>No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.</p>",
+    }
+    return res.render("sobre" , {sobre})
 })
 
 server.use(function(req, res) {
     res.status(404).render("notfound");
-});
+})
 
 server.listen(1717 , () => {
     console.log("servidor online papito")
